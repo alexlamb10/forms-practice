5a76e68d47181c42cf5d08a6e42e1fee8d3864f8
Alex Lamb <alexlamb104@gmail.com>
2023-06-14 16:08:38 -0600

    Added new button that links to simple form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SimpleFormComponent } from './simple-form/simple-form.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'simpleForm', component:SimpleFormComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
