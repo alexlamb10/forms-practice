554df63547853331f1bb5a9cd26dfa29900d84e1
Alex Lamb <alexlamb104@gmail.com>
2023-06-15 00:06:51 -0600

    Shows values on screen when successfully submitted

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-simple-form',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./simple-form.component.scss']
 })
 export class SimpleFormComponent {
+  public form = this._fb.group({
+    firstName: ["",[Validators.required]],
+    lastName: ["",[Validators.required]],
+    dob: ["",[Validators.required]],
+    subscribe: false,
+  })
 
+  public formUpdates$ = {}
+  // this.form.valueChanges.pipe(
+  //   startWith(this.form.value),
+  //   map((formValues) => {
+  //     return { ...formValues, valid: this.form.valid };
+  //   })
+  // );
+
+  public isValid = false;
+
+  submit() {
+    console.log(this.form.value)
+    this.isValid = this.form.valid;
+    this.formUpdates$ = this.form.value
+  }
+
+  constructor(private _fb: FormBuilder) { }
 }
